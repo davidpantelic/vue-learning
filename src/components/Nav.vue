1131c5e373815278103f55f23b7eeb0cf2e494d6
David Pantelic <davidpantelic1996@gmail.com>
2024-08-24 00:04:36 +0200

    added tailwind css

---
+++ src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/base.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
